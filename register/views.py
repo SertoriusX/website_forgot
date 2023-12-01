6ae83377dd3c385ff25f71d7e8547123b05f64e3
SertoriusX <152284852+SertoriusX@users.noreply.github.com>
2023-12-01 19:04:51 +0200

    Create views.py

---
+++ register/views.py
@@ -0,0 +1,43 @@
+# views.py
+
+
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django import forms
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+
+from django.contrib.auth.forms import AuthenticationForm #add this
+
+
+# Create your views here.
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/register')
+    else:
+        form=RegisterForm()
+    return render(response, "register/register.html", {"form":form})
+
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main:homepage")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="main/login.html", context={"login_form":form})
+
+
